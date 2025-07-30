fa26177277043a9bed829c2ff3d47e7265218249
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-07-30 12:51:18 +0000

    container config done

---
+++ JavaScript_LEarn/variables.js
@@ -0,0 +1,8 @@
+const id = 12345
+let stud_name = "Arun"
+var class_name = "Physics"
+console.table([id, stud_name, class_name]);
+/*
+It is not recommended  to use var keyword anymore and we cannot change the values of constants.
+*/
+console.table([id, stud_name, class_name]);
