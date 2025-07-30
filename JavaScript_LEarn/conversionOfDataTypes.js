d09d17e27e44bfb6b79a22c4129a1ad46b7263ad
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-07-30 14:15:04 +0000

    Data Types Conversion

---
+++ JavaScript_LEarn/conversionOfDataTypes.js
@@ -0,0 +1,16 @@
+let number = 1234
+let stringNum = "abcd"
+let number1 = Number(stringNum)
+
+console.log(number1); // gives the data types as NaN which is Not a Number
+
+let val = true
+let num = 1
+let finval = Boolean(num)
+console.log(finval);
+
+let numval = 11
+let strval = String(numval)
+console.log(strval);
+console.log(typeof(strval))
+
