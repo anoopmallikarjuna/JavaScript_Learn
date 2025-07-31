f201392abf49de5a20789da41bbe68611e17e559
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-07-31 13:21:48 +0000

    JS Basics

---
+++ JavaScript_basics/dateandtime.js
@@ -0,0 +1,12 @@
+const date1 = new Date("01-21-2023");
+
+
+console.log(date1);
+console.group(date1.toString());
+console.log(date1.toLocaleString());
+const date2 = new Date(2023, 0, 21, 3, 5);
+console.log(date2.toString());  
+
+
+
+
