2279390eeb62353821e3ca860a25d1638c38cbcf
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-08-01 07:16:27 +0000

    Scope in JS

---
+++ JS_Functions/function_scope.js
@@ -0,0 +1,23 @@
+//var keyword is not much used for the purpose of declaring variables as it disrupts the scope of the variable
+
+if(true){
+let a = 20;
+const b = 40;
+var c = 30;
+}
+
+//console.log(a);
+//console.log(b);
+console.log(c);
+
+//lets say I am going to declare a var variable outside a local scope and let's see what our output will be
+
+var a = 100
+
+if(true) {
+    let b = 20;
+    const c = 30;
+    a = 10;
+}
+
+console.log(a);
