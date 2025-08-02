7b2fb9b7b9bcd992fbbeba20a128a70cfcd302d8
anoopmallikarjuna <anoopmallikarjuna434@gmail.com>
2025-08-02 08:47:01 +0000

    Conditionals

---
+++ Control_Structures/conditionals.js
@@ -0,0 +1,15 @@
+const arr = []
+
+if(arr.length === 0) {
+    console.log("Array is empty")
+}
+
+const Obj = {}
+
+if(Object.keys(Obj).length === 0) {
+    console.log("Object is empty")
+}
+
+const obj1 = null ?? 10
+
+console.log(obj1);
